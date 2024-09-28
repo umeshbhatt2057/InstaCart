7775b01dc1d6af478cf729eae4c0b92ed7b73d30
Umesh Bhatt <100942158+umcBhatt@users.noreply.github.com>
2024-09-28 21:32:07 +0545

    limit 20mb

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ app.use(cors({
     origin : process.env.FRONTEND_URL,
     credentials : true
 }))
-app.use(express.json({limit : '5mb'}))
+app.use(express.json({limit : '20mb'}))
 app.use(cookieParser())
 
 app.use("/api",router)
